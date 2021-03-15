e15b41002399206814cef2cb5bfd545de8d9b359
Armando Arlan Joergensen <armando-a-j@hotmail.com>
2021-03-15 18:50:53 -0300

    Fix-Content-SideBar
    Removendo o componente de SideBar e Content da Home.

--- ignite-componentizando-a-aplicacao/src/components/Content.tsx
+++ ignite-componentizando-a-aplicacao/src/components/Content.tsx
@@ -1,3 +1,30 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "../components/MovieCard";
+
+interface Movie {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  movies: Movie[];
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="movies-list">
+      {props.movies.map((movie) => (
+        <MovieCard
+          title={movie.Title}
+          poster={movie.Poster}
+          runtime={movie.Runtime}
+          rating={movie.Ratings[0].Value}
+        />
+      ))}
+    </div>
+  );
 }
--- ignite-componentizando-a-aplicacao/src/components/SideBar.tsx
+++ ignite-componentizando-a-aplicacao/src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface Genre {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface SideBarProps {
+  list: Genre[];
+  onChange: (value: number) => void;
+  isSelected: number;
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.list.map((genre) => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.onChange(genre.id)}
+            selected={props.isSelected === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
