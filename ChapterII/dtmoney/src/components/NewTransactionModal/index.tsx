b6b902dbaa794b1369b9351ffa53ff46ec54eecd
Armando Arlan Joergensen <armando-a-j@hotmail.com>
2021-03-17 14:22:33 -0300

    fix-styles-modal
    Criando estilos globais para o modal

--- ChapterII/dtmoney/src/components/NewTransactionModal/index.tsx
+++ ChapterII/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
 import ReactModal from "react-modal";
 
+import closeImg from "../../assets/close.svg";
+
+import { Container } from "./styles";
+
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -10,8 +14,29 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <ReactModal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h1>oisasiash</h1>
+    <ReactModal
+      overlayClassName="react-modal-overlay"
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar transacao</h2>
+
+        <input type="text" placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+
+        <input type="text" placeholder="Título" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
     </ReactModal>
   );
 }
