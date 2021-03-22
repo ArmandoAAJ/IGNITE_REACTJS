fd8e0a537b10944d367385b84164ce975f4d6cbf
Armando Arlan Joergensen <armando-a-j@hotmail.com>
2021-03-21 22:36:02 -0300

    Fix-Add-To-Cart
    Adicionando item ao carrinho e adicionando item no localStorage

--- ignite-reactjs-criando-um-hook-de-carrinho-de-compras/src/hooks/useCart.tsx
+++ ignite-reactjs-criando-um-hook-de-carrinho-de-compras/src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,20 +23,42 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
-    try {
-      // TODO
+    try {      
+      const productExist = cart.filter((product) => product.id === productId);
+
+      if (productExist.length < 1) {
+        const storagedCart = localStorage.getItem("@RocketShoes:cart");
+        const response = await api.get("/products");
+        const product = response.data.filter(
+          (product: Product) => product.id === productId
+        );
+
+        if (storagedCart) {
+          const storagedCartParsed = JSON.parse(storagedCart);
+          storagedCartParsed.push(product[0]);
+
+          localStorage.setItem(
+            "@RocketShoes:cart",
+            JSON.stringify(storagedCartParsed)
+          );
+          setCart(storagedCartParsed);
+        } else {
+          localStorage.setItem("@RocketShoes:cart", JSON.stringify(product));
+          setCart(product);
+        }
+      }
     } catch {
-      // TODO
+      console.tron.log("merdaaaaa");
     }
   };
 
