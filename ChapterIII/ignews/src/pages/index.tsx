89e601c141ced4ada83f524e41e7571bcaa1dc15
Armando Arlan Joergensen <armando-a-j@hotmail.com>
2021-03-27 23:16:16 -0300

    Create header-component
    Criando o componente de Header da aplicacao

--- ChapterIII/ignews/src/pages/index.tsx
+++ ChapterIII/ignews/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "../styles/home.modules.scss";
 
 export default function Home() {
   return (
@@ -7,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Index Ig.news</title>
       </Head>
-      <h1 className={styles.title}>Helo Word</h1>
+      <h1>Helo Word</h1>
     </>
   );
 }
