6f5cecd7c037a741e5da7e23c5e91c2f44d40cfa
Armando Arlan Joergensen <armando-a-j@hotmail.com>
2021-03-28 00:33:17 -0300

    Update home-server-side
    Utilizando stripe para buscar dados e utilizando ServerSideProps.

--- ChapterIII/ignews/src/components/SubscribeButton/index.tsx
+++ ChapterIII/ignews/src/components/SubscribeButton/index.tsx
@@ -1,6 +1,12 @@
 import styles from "./styles.module.scss";
 
-export const SubscribeButton: React.FC = () => {
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export const SubscribeButton = ({
+  priceId,
+}: SubscribeButtonProps) => {
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
