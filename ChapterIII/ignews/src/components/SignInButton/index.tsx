9df0a64295cf2c3aafb9e11f298dc46e806ac4af
Armando Arlan Joergensen <armando-a-j@hotmail.com>
2021-03-28 17:54:39 -0300

    Update
    Login com github

--- ChapterIII/ignews/src/components/SignInButton/index.tsx
+++ ChapterIII/ignews/src/components/SignInButton/index.tsx
@@ -1,18 +1,28 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, useSession, signOut } from "next-auth/client";
+
 import styles from "./styles.module.scss";
 
 export const SignInButton: React.FC = () => {
-  const isUserLoggedIn = true;
-
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  const [session] = useSession();
+  
+  return session ? (
+    <button
+      onClick={() => signOut()}
+      type="button"
+      className={styles.signInButton}
+    >
       <FaGithub color="#04d361" />
-      ArmandoAAJ
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      onClick={() => signIn("github")}
+      type="button"
+      className={styles.signInButton}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
