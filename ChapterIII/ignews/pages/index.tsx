0ad1939981b5b35729d18abdf287930948e888fb
Armando Arlan Joergensen <armando-a-j@hotmail.com>
2021-03-27 22:16:50 -0300

    Create Fonts
    Carregando fonts personalizadas no _document e adicionando Header por página

--- ChapterIII/ignews/pages/index.tsx
+++ ChapterIII/ignews/pages/index.tsx
@@ -1,5 +1,13 @@
+import Head from "next/head";
 import styles from "../styles/home.modules.scss";
 
 export default function Home() {
-  return <h1 className={styles.title}>Helo Word</h1>;
+  return (
+    <>
+      <Head>
+        <title>Index Ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Helo Word</h1>
+    </>
+  );
 }
