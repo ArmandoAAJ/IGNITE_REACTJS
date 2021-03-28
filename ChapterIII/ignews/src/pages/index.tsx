06cd1dee7e83699b1782edec7dae88022ebd7426
Armando Arlan Joergensen <armando-a-j@hotmail.com>
2021-03-27 23:55:06 -0300

    Create component-button
    criando o componenete do botao de subcribe da home

--- ChapterIII/ignews/src/pages/index.tsx
+++ ChapterIII/ignews/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 import styles from "./home.module.scss";
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
       </Head>
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-          <span>👏  Hey, welcome</span>
+          <span>👏 Hey, welcome</span>
           <h1>
             News about the <span>React</span> world.
           </h1>
@@ -17,6 +18,7 @@ export default function Home() {
             Get acess to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="" />
       </main>
