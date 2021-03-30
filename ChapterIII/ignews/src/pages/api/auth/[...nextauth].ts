d4b2a690f2a2a1c28b771906183d5d701db8af03
Armando Arlan Joergensen <armando-a-j@hotmail.com>
2021-03-30 16:30:10 -0300

    Update
    Regredindo a versao de tipagens next-auth com problemas

--- ChapterIII/ignews/src/pages/api/auth/[...nextauth].ts
+++ ChapterIII/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,38 +1,111 @@
-import { query as q } from 'faunadb';
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+import NextAuth, { User } from "next-auth";
+import Providers from "next-auth/providers";
 
-import { fauna } from '../../../services/fauna';
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
+
+import { github } from "../../../services/github";
+
+interface GitHubUserEmail {
+  primary: boolean;
+  email: string;
+}
+
+interface UserSession extends User {
+  accessToken: string;
+}
+
+export async function getUserEmail(accessToken: string) {
+  const response = await github.get("user/emails", {
+    headers: {
+      Authorization: `token ${accessToken}`,
+    },
+  });
+
+  const { email } = response.data.find(
+    (email: GitHubUserEmail) => email.primary
+  );
+
+  return email;
+}
 
 export default NextAuth({
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user',
+      scope: "read:user,user:email",
     }),
   ],
+  session: {
+    jwt: true,
+  },
+  jwt: {
+    signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
+  },
   callbacks: {
+    async jwt(token, user, account) {
+      if (account) {
+        token.accessToken = account.accessToken;
+      }
+
+      return token;
+    },
+    async session(session, user: UserSession) {
+      if (!session.user.email) {
+        session.user.email = await getUserEmail(user.accessToken);
+      }
+
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
     async signIn(user, account, profile) {
-      const { email } = user;
+      const email = user.email;
 
       try {
         await fauna.query(
           q.If(
             q.Not(
-              q.Exists(
-                q.Match(q.Index('user_by_email'), q.Casefold(user.email)),
-              ),
+              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email)))
             ),
-            q.Create(q.Collection('users'), {
-              data: { email },
-            }),
-            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email))),
-          ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+          )
         );
 
         return true;
-      } catch {
+      } catch (error) {
+        console.error(error);
+
         return false;
       }
     },
--- ChapterIII/ignews/src/pages/posts/[slug].tsx
+++ ChapterIII/ignews/src/pages/posts/[slug].tsx
@@ -1,68 +1,82 @@
-import { GetServerSideProps } from "next";
-import Head from "next/head";
-import { getSession } from "next-auth/client";
-import { RichText } from "prismic-dom";
-import { getPrismicClient } from "../../services/prismic";
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
 
-import styles from "./post.module.scss";
+import { getSession } from 'next-auth/client'
+
+import { RichText } from 'prismic-dom'
+
+import { getPrismicClient } from '../../services/prismic'
+
+import styles from './post.module.scss'
 
 interface PostProps {
   post: {
-    slug: string;
-    title: string;
-    content: string;
-    update: string;
-  };
+    slug: string
+    title: string
+    content: string
+    updatedAt: string
+  }
 }
 
 export default function Post({ post }: PostProps) {
   return (
     <>
       <Head>
-        <title>{post.title} | IGNEWS</title>
+        <title>{post.title} | Ignews</title>
       </Head>
+
       <main className={styles.container}>
         <article className={styles.post}>
           <h1>{post.title}</h1>
-          <time>{post.update}</time>
+          <time>{post.updatedAt}</time>
           <div
             className={styles.postContent}
             dangerouslySetInnerHTML={{ __html: post.content }}
-          ></div>
+          />
         </article>
       </main>
     </>
-  );
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({
   req,
-  params,
+  params
 }) => {
-  const session = await getSession({ req });
-  const { slug } = params;
+  const session = await getSession({ req })
 
-  const prismic = getPrismicClient(req);
+  const { slug } = params
 
-  const response = await prismic.getByUID("publication", String(slug), {});
+  if (!session?.activeSubscription) {
+    return {
+      redirect: {
+        destination: `/posts/preview/${slug}`,
+        permanent: false
+      }
+    }
+  }
+
+  const prismic = getPrismicClient(req)
+
+  const response = await prismic.getByUID('post', String(slug), {})
 
   const post = {
     slug,
     title: RichText.asText(response.data.title),
     content: RichText.asHtml(response.data.content),
-    updated: new Date(response.last_publication_date).toLocaleDateString(
-      "pt-BR",
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      'pt-BR',
       {
-        day: "2-digit",
-        month: "long",
-        year: "numeric",
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
       }
-    ),
-  };
+    )
+  }
 
   return {
     props: {
-      post,
-    },
-  };
-};
+      post
+    }
+  }
+}
