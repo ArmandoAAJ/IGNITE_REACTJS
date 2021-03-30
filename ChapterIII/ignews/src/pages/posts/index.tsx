61e0f0c221234e96f009c49197d2f96340b7e6aa
Armando Arlan Joergensen <armando-a-j@hotmail.com>
2021-03-29 21:10:26 -0300

    Update Posts
    Listando os dados formatados

--- ChapterIII/ignews/src/pages/posts/index.tsx
+++ ChapterIII/ignews/src/pages/posts/index.tsx
@@ -1,40 +1,49 @@
 import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 import { GetStaticProps } from "next";
+import Link from "next/link";
 import Head from "next/head";
+
 import { getPrismicClient } from "../../services/prismic";
+
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
-        <title>POST | IGNEWS</title>
+        <title>Posts | Ig.news</title>
       </Head>
+
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>12 de Marco de 2021</time>
-            <strong>asihdiuahd iaushdiasud iasdhaiusdh</strong>
-            <p>
-              dgaiusdgas ausdausd idsasiud isdhiasud dsbaiusdiasd
-              daisdbiashdasdv asdugduasgdusGADS AdaSGDUAyhdghsdvuydgwquydh
-            </p>
-          </a>
-          <a href="">
-            <time>12 de Marco de 2021</time>
-            <strong>asihdiuahd iaushdiasud iasdhaiusdh</strong>
-            <p>
-              dgaiusdgas ausdausd idsasiud isdhiasud dsbaiusdiasd
-              daisdbiashdasdv asdugduasgdusGADS AdaSGDUAyhdghsdvuydgwquydh
-            </p>
-          </a>
+          {posts.map((post) => (
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
   );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps<PostsProps> = async () => {
   const prismic = getPrismicClient();
 
   const response = await prismic.query(
@@ -42,9 +51,27 @@ export const getStaticProps: GetStaticProps = async () => {
     { fetch: ["publication.title", "publication.content"], pageSize: 100 }
   );
 
-  console.log(JSON.stringify(response, null, 2));
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: {
+      posts,
+    },
   };
 };
