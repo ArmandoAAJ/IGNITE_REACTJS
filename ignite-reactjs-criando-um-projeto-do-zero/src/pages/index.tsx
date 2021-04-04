f3e6a6c110a9cea1061ea64b3e547c339bfa7ea2
Armando Arlan Joergensen <armando-a-j@hotmail.com>
2021-04-04 18:20:20 -0300

    Create list_posts
    Buscando e listando os posts do prismic

--- ignite-reactjs-criando-um-projeto-do-zero/src/pages/index.tsx
+++ ignite-reactjs-criando-um-projeto-do-zero/src/pages/index.tsx
@@ -1,5 +1,9 @@
+import { useState } from 'react';
 import { GetStaticProps } from 'next';
+import { FiUser, FiCalendar } from 'react-icons/fi';
 
+import { RichText } from 'prismic-dom';
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +28,66 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const [posts, setPosts] = useState(postsPagination.results);
+  const [postsNextPage, setPostsNextPage] = useState(postsPagination.next_page);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  return (
+    <main className={commonStyles.contentContainer}>
+      <div className={styles.logo}>
+        <img src="/logo.png" alt="logo" />
+        <img src="/spacetraveling.png" alt="spacetraveling" />
+        <div />
+      </div>
+      <div className={styles.content}>
+        {posts.map(post => (
+          <a href="/" key={post.uid}>
+            <strong>{post.data.title}</strong>
+            <p>{post.data.subtitle}</p>
+            <div>
+              <FiCalendar />
+              <time>{post.first_publication_date}</time>
+              <FiUser style={{ marginLeft: 31 }} />
+              <time>{post.data.author}</time>
+            </div>
+          </a>
+        ))}
+      </div>
+    </main>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    { fetch: ['post.title', 'post.author', 'post.subtitle'], pageSize: 1 }
+  );
+
+  const results = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: new Date(
+        post.last_publication_date
+      ).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+      data: {
+        title: RichText.asText(post.data.title),
+        subtitle: RichText.asText(post.data.subtitle),
+        author: RichText.asText(post.data.author),
+      },
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: postsResponse.next_page,
+        results,
+      },
+    },
+  };
+};
